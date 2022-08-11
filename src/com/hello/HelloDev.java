c734ac284cef75b911085e69d0baaca2d022d28c
maheswar2 <35700643+maheswar2@users.noreply.github.com>
2022-08-11 15:55:46 +0530

    Update HelloDev.java

--- src/com/hello/HelloDev.java
+++ src/com/hello/HelloDev.java
@@ -2,7 +2,7 @@ package com.hello;
 
 public class HelloDev {
     public static void main(String[] args) {
-        System.out.println("this is demo class");
+        System.out.println("this is demo class fo demo");
         //coment added by team lead
     }
 }
