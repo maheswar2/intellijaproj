38724928178e426c368d44729d084729cc986984
maheswar2 <35700643+maheswar2@users.noreply.github.com>
2022-08-11 15:52:54 +0530

    Update HelloDev.java
    comment added by team lead for identify changes

--- src/com/hello/HelloDev.java
+++ src/com/hello/HelloDev.java
@@ -3,5 +3,6 @@ package com.hello;
 public class HelloDev {
     public static void main(String[] args) {
         System.out.println("this is demo class");
+        //coment added by team lead
     }
 }
